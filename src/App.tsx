9f2c4e8fe2d92ec93f29dd3651e46ef2b93bcae7
jnfran92 <jnfran92@gmail.com>
2022-06-03 16:30:16 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function getData(name: String) {
+function getRender(name: String) {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1>Reactcoin</h1>
                 <h2>Crypto matters!</h2>
-                <h3>name: {name}</h3>
+                <h3>hello: {name}</h3>
             </header>
         </div>
         )
@@ -28,9 +28,8 @@ class App extends React.Component<AppProps> {
     componentWillUnmount() {
     }
 
-
     render() {
-        return getData(this.props.oneThing);
+        return getRender(this.props.oneThing);
     }
 }
 
