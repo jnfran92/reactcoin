66e3045915649f67e79132dd834195b93c57bc45
jnfran92 <jnfran92@gmail.com>
2022-06-03 16:06:01 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
+const data = (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Reactcoin</h1>
-        <h2>Crypto matters!</h2>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Reactcoin</h1>
+            <h2>Crypto matters!</h2>
+        </header>
     </div>
-  );
+);
+
+class App extends React.Component {
+    render() {
+        return data;
+    }
 }
 
 export default App;
